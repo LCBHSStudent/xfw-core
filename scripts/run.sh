604b375f0cb6c42360933db6bb5bb6e5954b2679
LCBHSStudent <swordland288@gmail.com>
2022-09-20 16:39:58 +0800

    fix(homo-space, scripts): fix bug in embedded homo-space && fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,10 +3,10 @@
 pushd /xfw/xfw-core/bin
 
 service mysql start
-nohup ./go-cqhttp > cqhttp.log &2>1 &
+nohup ./go-cqhttp > cqhttp.log 2>&1 &
 sleep 10 
-nohup ./xfw-core > xfw.log &2>1 &
-nohup ./homo-space > homo.log &2>1 &
+nohup ./xfw-core > xfw.log 2>&1 &
+nohup ./homo-space > homo.log 2>&1 &
 
 popd
 
